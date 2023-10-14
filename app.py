376d82ab939808a2b9b0899527e2d79eaa9ebc0c
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-14 14:49:12 +0300

    adding index explict and get_data methods

--- app.py
+++ app.py
@@ -1,17 +1,73 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    }
+]
+
 @app.route("/")
-
 def index():
-
     return "<h1>hello world</h1>"
 
 @app.route("/no_content")
-
 def no_content():
     """return 'No content found' with a status of 204
+
     Returns:
         string: No content found
         status code: 204
@@ -19,3 +75,26 @@ def no_content():
     response = jsonify({"status": "No content found"})
     response.status_code = 204
     return response
+
+
+@app.route("/exp")
+def index_explicit():
+    """return 'Hello World' message with a status code of 200
+
+    Returns:
+        string: Hello World
+        status code: 200
+    """
+    response = make_response({"message":"Hello World"})
+    response.status_code = 200
+    return response
+
+@app.route("/data")
+def get_data():
+    try:
+        if data and len(data) > 0:
+            return{"message": f"Data of lenght {len(data)} found"}
+        else:
+            return{"message": "Data is empty"}, 500
+    except NameError:
+        return{"message": "Data not found"}, 404
